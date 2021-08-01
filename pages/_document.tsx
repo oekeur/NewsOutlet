0ba15359e846b393355a8292740020ba2255a465
Oscar Keur <o.e.keur@gmail.com>
2021-08-01 19:31:55 +0200

    Add Roboto as a decent baseline

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,13 +7,19 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link
-                        rel="preload"
-                        href="/fonts/some-font/some-font-regular-400.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="anonymous"
-                    /> */}
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="true"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />

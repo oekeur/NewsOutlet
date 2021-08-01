5a969ce8bb5073022ff9faf31aaee47689a67c79
Oscar Keur <o.e.keur@gmail.com>
2021-08-01 15:26:41 +0200

    Add homepage content in addition to posts
    pages.ts can serve as a simple source for content for the page(s) itself, let's use that

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import useSWR from "swr";
+import Head from "next/head";
+import pages from "../mock/pages";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+export default function Home({ posts, homepageContent }) {
+    const { title, description } = homepageContent;
 
 export default function Home(posts) {
     const { data, error } = useSWR("/api/home", fetcher);
@@ -10,6 +12,14 @@ export default function Home(posts) {
 
     return (
         <div>
+            <Head>
+                <title>{title}</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <meta name="description" content={description} />
+            </Head>
             <h1>This is the homepage</h1>
         </div>
     );
@@ -20,10 +30,12 @@ export async function getStaticProps() {
     // Call an external API endpoint to get posts
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROOT}home`);
     const posts = await res.json();
+    const homepageContent = pages.data[0];
 
     return {
         props: {
             posts,
+            homepageContent,
         },
     };
 }
